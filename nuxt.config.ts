bc7c2c65b7e4b9eb0b9e9ab14ac3b4c4e083a1cc
Mavlon4658 <mavlonbe4658>
2024-08-27 17:55:21 +0500

    fixed bugs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+export default {
+  ssr: true,
+  target: 'server',
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss'],
@@ -15,4 +17,4 @@ export default defineNuxtConfig({
       
     }
   }
-})
+}
