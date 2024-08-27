ae65709a3010536b67fc27a885c075bd85714969
Mavlon4658 <mavlonbe4658>
2024-08-27 18:52:48 +0500

    remove ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default {
-  ssr: true,
-  target: 'server',
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss'],
