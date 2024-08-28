03846706b8418e35a7739b0cf93dadbe76c3a971
Mavlon4658 <mavlonbe4658>
2024-08-28 15:37:24 +0500

    account pages are ready

---
+++ layouts/auth.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="min-h-[100svh] bg-color-6">
+        <slot />
+    </div>
+</template>
---
+++ pages/sign-up.vue
@@ -0,0 +1,12 @@
+<script setup>
+definePageMeta({
+    layout: "auth",
+});
+</script>
+
+<template>
+    <div>
+        <p>Для регистрации и полному доступу к сайту</p>
+        <h2>Заполните форму</h2>
+    </div>
+</template>
